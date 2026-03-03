7698cdd2aa783116e0ba8c7e69168b26e4df3a22
YouTaegeun <dbxorms2310@naver.com>
2026-03-03 11:20:53 +0900

    fix: WishController.addWish()에 누락된 @Valid 어노테이션 복구

--- src/main/java/gift/wish/WishController.java
+++ src/main/java/gift/wish/WishController.java
@@ -3,6 +3,7 @@ package gift.wish;
 import gift.auth.AuthenticationResolver;
 import gift.member.Member;
 
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -46,7 +47,7 @@ public class WishController {
     @PostMapping
     public ResponseEntity<WishResponse> addWish(
         @RequestHeader("Authorization") String authorization,
-        @RequestBody WishRequest request
+        @Valid @RequestBody WishRequest request
     ) {
         // check auth
         Member member = authenticationResolver.extractMember(authorization);
